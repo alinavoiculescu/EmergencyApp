3b2e2d549348d21bce1f5a223f67f5d87487d757
GemanAmalia <amalia_alexa00@yahoo.com>
2022-05-04 18:58:59 +0300

    Update MainActivity ActionBar
    Update MainActivity

--- EmergencyAppSignUp/app/src/main/java/com/lifeSavers/emergencyappsignup/MainActivity.kt
+++ EmergencyAppSignUp/app/src/main/java/com/lifeSavers/emergencyappsignup/MainActivity.kt
@@ -3,16 +3,23 @@ package com.lifeSavers.emergencyappsignup
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
+import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import com.lifeSavers.emergencyappsignup.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
+    // ActionBar
+    private lateinit var actionBar: ActionBar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        actionBar = supportActionBar!!
+        actionBar.title = "Home"
 
         binding.profileBtn.setOnClickListener {
             startActivity(Intent(this, ProfileActivity::class.java))
